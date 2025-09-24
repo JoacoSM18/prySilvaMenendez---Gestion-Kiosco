517bb87a78b05a70a4ec476ad1a7550b733ecd13
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-24 12:30:08 -0300

    Arreglos

--- prySilvaMenendez-EjercicioSemana2/frmGestionKiosco.cs
+++ prySilvaMenendez-EjercicioSemana2/frmGestionKiosco.cs
@@ -13,28 +13,40 @@ namespace prySilvaMenendez_EjercicioSemana2
         {
             if (btnModificar.Checked)
             {
-                lblAgregado.Show();
-                txtAgregado.Show();
-                
+                lblAgregado.Visible = true;
+                txtAgregado.Visible = true;
             }
-            
-                
-            
+            else
+            {
+                lblAgregado.Enabled = false;
+                txtAgregado.Enabled = false;
+            }
+
+
+
 
         }
 
         private void btnAceptar_Click(object sender, EventArgs e)
-        { 
-
+        {
+            if (txtAgregado.Text == "" || txtCodigo.Text == "" || txtNombre.Text == "")
+            {
+                
                 if (btnAgregar.Checked)
+                {
                     MessageBox.Show("Producto Agregado Correctamente");
+                }
                 if (btnEliminar.Checked)
+                {
                     MessageBox.Show("Producto Eliminado Correctamente");
+                }
                 if (btnModificar.Checked)
+                {
                     MessageBox.Show("Producto Modificado Correctamente");
-
+                }
             }
 
         }
     }
+}
 
